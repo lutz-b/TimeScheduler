59f2ac928084ee01425e0650f53a509480ee90b0
DESKTOP-V49DORL\LBran <lbrandt@gmx.de>
2018-06-12 21:58:41 +0200

    project start

--- TimeScheduler.Local/Program.cs
+++ TimeScheduler.Local/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using Newtonsoft;
+using Newtonsoft.Json;
+using TimeScheduler.Models;
+using System.IO;
+
 
 namespace TimeScheduler.Local
 {
@@ -6,6 +11,12 @@ namespace TimeScheduler.Local
     {
         static void Main(string[] args)
         {
+            var file = @"C:\Work\TimeScheduler\TimeScheduler.Models\LisasPlan.json";
+            var content = File.ReadAllText(file);
+            var monday = JsonConvert.DeserializeObject<Day>(content);
+
+
+
             Console.WriteLine("Hello World!");
         }
     }
---
+++ TimeScheduler.Models/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TimeScheduler.Models
+{
+    public class Day
+    {
+        public DayOfWeek DayOfWeek { get; set; }
+        public List<Event> Events { get; set; }
+    }
+
+    public class Event
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public TimeRange TimeRange { get; set; }
+
+    }
+
+    public class TimeRange
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+    }
+
+
+}
