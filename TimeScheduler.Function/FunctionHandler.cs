9a62104118ebfd9c7e781ba095c5c19fe04be72b
lutz.brandt <lutz.brandt@doctari.de>
2018-10-26 20:31:52 +0200

    implemented startup and handler

---
+++ TimeScheduler.Function/FunctionHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using OpenFaaS.Dotnet;
+
+namespace TimeScheduler.Function
+{
+    public class FunctionHandler : BaseFunction
+    {
+        public FunctionHandler(IFunctionContext functionContext) 
+            : base(functionContext)
+        {
+        }
+
+        public override void Handle(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TimeScheduler.Local/Program.cs
+++ TimeScheduler.Local/Program.cs
@@ -3,7 +3,8 @@ using Newtonsoft;
 using Newtonsoft.Json;
 using TimeScheduler.Models;
 using System.IO;
-
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
 
 namespace TimeScheduler.Local
 {
@@ -11,10 +12,21 @@ namespace TimeScheduler.Local
     {
         static void Main(string[] args)
         {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+        }
+
+        private static void Test()
+        {
             var file = @"C:\Work\TimeScheduler\TimeScheduler.Models\LisasPlan.json";
             var content = File.ReadAllText(file);
             var monday = JsonConvert.DeserializeObject<Day>(content);
-
 
 
             Console.WriteLine("Hello World!");
