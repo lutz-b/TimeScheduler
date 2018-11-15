918e1c7e38fc42d8756174ae02bdc93b08f7f589
lutz.brandt <lutz.brandt@doctari.de>
2018-11-15 14:39:38 +0100

    working on backend

--- TimeScheduler.Function/FunctionHandler.cs
+++ TimeScheduler.Function/FunctionHandler.cs
@@ -20,8 +20,30 @@ namespace TimeScheduler.Function
             var skillRequest = JsonConvert.DeserializeObject<SkillRequest>(input);
             SkillResponse response = null;
 
-            //response = ResponseBuilder.Tell($"Request-Typ {requestType.Name}");
-            response = ResponseBuilder.Tell("Unbekannte Anfrage.");
+
+
+
+
+            if (skillRequest.Request.Type == "LaunchRequest")
+            {
+                response = ResponseBuilder.Tell("Willkommen zum Tech-Weekly.");
+            }
+            else if (skillRequest.Request.Type == "IntentRequest")
+            {
+                var date = DateTime.Now.Date;
+
+
+                var anwser = new SsmlOutputSpeech
+                {
+                    Ssml = $"<speak>Heute ist<say-as interpret-as=\"date\">{date:dddd}</say-as></speak>"
+                    //Ssml = "<speak>Morgen ist <break> <amazon:effect name = \"whispered\"> Freitag</amazon:effect></speak>"
+                    //Ssml = "<speak>Anderer Text<amazon:effect name = \"whispered\"> Text ganz leise.</amazon:effect>.Weiterer Text</speak>"
+                };
+
+                response = ResponseBuilder.Tell(anwser);
+            }
+
+            
 
             Context.WriteContent(JsonConvert.SerializeObject(response, Formatting.Indented,
                 new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() }));
